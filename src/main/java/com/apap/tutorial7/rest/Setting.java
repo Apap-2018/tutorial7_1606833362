3c68eae9eb76c7140f40f4bbd8a30dba4c095c83
Nabilla Hariyana <nabilla.hari@gmail.com>
2018-10-31 17:51:35 +0700

    Initial commit and finished task 1

---
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -0,0 +1,6 @@
+package com.apap.tutorial7.rest;
+
+public class Setting {
+	final public static String pilotURl = "https://f215f2e8-9709-4dc9-b002-836d1d3c207f.mock.pstmn.io";
+
+}
---
+++ src/main/java/com/apap/tutorial7/service/FlightServiceImpl.java
@@ -0,0 +1,56 @@
+package com.apap.tutorial7.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apap.tutorial7.model.FlightModel;
+import com.apap.tutorial7.repository.FlightDb;
+
+/**
+ * FlightServiceImpl
+ */
+@Service
+@Transactional
+public class FlightServiceImpl implements FlightService {
+    @Autowired
+    private FlightDb flightDb;
+    
+    @Override
+    public FlightModel addFlight(FlightModel flight) {
+        return flightDb.save(flight);
+    }
+
+    @Override
+    public void deleteByFlightNumber(String flightNumber) {
+        flightDb.deleteByFlightNumber(flightNumber);
+    }
+
+    @Override
+    public Optional<FlightModel> getFlightDetailByFlightNumber(String flightNumber) {
+        return flightDb.findByFlightNumber(flightNumber);
+    }
+
+	@Override
+	public Optional<FlightModel> getFlightDetailByFlightId(long flightId) {
+		return flightDb.findById(flightId);
+	}
+
+	@Override
+	public void updateFlight(long flightId, FlightModel flight) {
+		flightDb.save(flight);
+	}
+
+	@Override
+	public void deleteFlight(FlightModel flight) {
+		flightDb.delete(flight);
+	}
+	
+	@Override
+	public List<FlightModel> getFlights() {
+		return flightDb.findAll();
+	}
+}
